fce56d5271808b947474f981d28953b74552a5ee
Don Button <37458717+DigitalGizmo@users.noreply.github.com>
2024-07-16 13:38:01 -0400

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,39 @@
+<script>
+  let frame = 0;
+  let modeIdx = 0;
+
+  function setView(_modeIdx) {
+    modeIdx = _modeIdx;
+  }
+</script>
+
+<main>
+  <div>
+    {#if modeIdx === 0}
+      <img width="60%" src="images/rotate/Lombard00{frame}.png" alt="">
+      <p>you are here. Frame: {frame}</p>
+      Rotate: 
+      <input type="range" min="0" max="9" bind:value={frame} />
+    {/if}
+    {#if modeIdx === 1}
+      <img width="60%" src="images/views/crawler-track.png" alt="">
+    {/if}
+
+  </div>
+
+  <ul>
+    <li><a href="/"
+      on:click={(e) => { e.preventDefault(); setView(1);}}>
+      Treads</a></li>
+    <li>Engine</li>
+    <li>Steering</li>
+    <li>Brakes</li>
+  </ul>
+  <p>back to <a href="/"
+    on:click={(e) => { e.preventDefault(); setView(0);}}>
+    main view</a>
+    - modeIdx: {modeIdx}
+  </p>
+
+</main>
+
