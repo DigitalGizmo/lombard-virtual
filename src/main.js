0fea8a3a4495786e1627ec84e7d4d2ab6592db63
Don Button <37458717+DigitalGizmo@users.noreply.github.com>
2024-07-23 10:17:07 -0400

    add and switch to model

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import './app.css'
 import App from './App.svelte'
+import Model from './Model.svelte'
 
-const app = new App({
+// const app = new App({
+const app = new Model({
   target: document.getElementById('app'),
 })
 
