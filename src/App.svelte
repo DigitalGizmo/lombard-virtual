3d4fc55a47c66dfb18d376af3f3188b4e37d3da9
Juliet Jacobson <julietanna@users.noreply.github.com>
2024-10-23 13:00:06 -0400

    beginning of new layout

--- src/App.svelte
+++ src/App.svelte
@@ -42,21 +42,21 @@
       {/if}
       {#if viewIdx === 1}
         <img src="{assetPath}images/views/crawler-track.png" alt="lombard crawler track">
-        <p>see it crawl [button]</p>
+        <p class="controls">see it crawl [button]</p>
       {/if}
       {#if viewIdx === 2}
         <img src="{assetPath}images/views/skis.png" alt="lombard steering">
-        <p>see it steer [button]</p>
+        <p class="controls">see it steer [button]</p>
       {/if}
       {#if viewIdx === 3}
         <img src="{assetPath}images/views/engine.png" alt="lombard gas engine">
-        <p>see it rev [button]</p>
+        <p class="controls">see it rev [button]</p>
       {/if}
       {#if viewIdx === 4}
         <img src="{assetPath}images/views/brakes.png" alt="lombard brakes">
-        <p>see it stop [button]</p>
+        <p class="controls">see it stop [button]</p>
       {/if}
-    </div>
+    </div><!-- /#main -->
 
     <!-- text -->
     <div class="content {views[viewIdx].slug}">
@@ -67,59 +67,58 @@
         on:click={(e) => { e.preventDefault(); showModal();}}>
         Learn more</a>
       </p>
-    </div>
+    </div><!-- /content -->
+
+    <nav class="menu">
+      <ul>
+        <li>
+          <a href="/"
+            on:click={(e) => { 
+              e.preventDefault(); setView(0);
+              setPreview;
+              console.log('isPreview: ' + isPreview)
+            }}>
+            <h3 class="{(viewIdx === 0) ? 'nav-selected' : ''}">Overview/Start</h3>
+            <img src="{assetPath}images/views/lombard-thm.png" alt="lombard hauler 3D view">
+          </a>
+        </li>
+        <li>
+          <a href="/"
+            on:click={(e) => { e.preventDefault(); setView(1);}}>
+            <h3 class="{(viewIdx === 1) ? 'nav-selected' : ''}">Crawler Track</h3>
+            <img src="{assetPath}images/views/crawler-track-thm.png" alt="lombard crawler track">
+          </a>
+        </li>
+        <li>
+          <a href="/"
+            on:click={(e) => { e.preventDefault(); setView(2);}}>
+            <h3 class="{(viewIdx === 2) ? 'nav-selected' : ''}">Steering Skis</h3> 
+            <img src="{assetPath}images/views/skis-thm.png" alt="lombard steering">           
+          </a>
+        </li>
+        <li>
+          <a href="/"
+            on:click={(e) => { e.preventDefault(); setView(3);}}>
+            <h3 class="{(viewIdx === 3) ? 'nav-selected' : ''}">Gas Engine</h3>
+            <img src="{assetPath}images/views/engine-thm.png" alt="lombard gas engine">
+          </a>
+        </li>
+        <li>
+          <a href="/"
+            on:click={(e) => { e.preventDefault(); setView(4);}}>
+            <h3 class="{(viewIdx === 4) ? 'nav-selected' : ''}">Brake System</h3>
+            <img src="{assetPath}images/views/brakes-thm.png" alt="lombard hauler 3D view">
+          </a>
+        </li>
+      </ul>
+    </nav>
+    {#if isModalShowing}
+      <MoreModal 
+        assetPath={assetPath} 
+        viewIdx={viewIdx}
+        bind:isModalShowing 
+      />
+    {/if}
+
   </main>
-
-  <nav class="menu">
-    <ul>
-      <li>
-        <a href="/"
-          on:click={(e) => { 
-            e.preventDefault(); setView(0);
-            setPreview;
-            console.log('isPreview: ' + isPreview)
-          }}>
-          <img src="{assetPath}images/views/lombard-thm.png" alt="lombard hauler 3D view">
-          <h3 class="{(viewIdx === 0) ? 'nav-selected' : ''}">Overview/Start</h3>  
-        </a>
-      </li>
-      <li>
-        <a href="/"
-          on:click={(e) => { e.preventDefault(); setView(1);}}>
-          <img src="{assetPath}images/views/crawler-track-thm.png" alt="lombard crawler track">
-          <h3 class="{(viewIdx === 1) ? 'nav-selected' : ''}">Crawler Track</h3>
-        </a>
-      </li>
-      <li>
-        <a href="/"
-          on:click={(e) => { e.preventDefault(); setView(2);}}>
-          <img src="{assetPath}images/views/skis-thm.png" alt="lombard steering">
-          <h3 class="{(viewIdx === 2) ? 'nav-selected' : ''}">Steering Skis</h3>            
-        </a>
-      </li>
-      <li>
-        <a href="/"
-          on:click={(e) => { e.preventDefault(); setView(3);}}>
-          <img src="{assetPath}images/views/engine-thm.png" alt="lombard gas engine">
-          <h3 class="{(viewIdx === 3) ? 'nav-selected' : ''}">Gas Engine</h3>
-        </a>
-      </li>
-      <li>
-        <a href="/"
-          on:click={(e) => { e.preventDefault(); setView(4);}}>
-          <img src="{assetPath}images/views/brakes-thm.png" alt="lombard hauler 3D view">
-          <h3 class="{(viewIdx === 4) ? 'nav-selected' : ''}">Brake System</h3>
-        </a>
-      </li>
-    </ul>
-  </nav>
-
-  {#if isModalShowing}
-    <MoreModal 
-      assetPath={assetPath} 
-      viewIdx={viewIdx}
-      bind:isModalShowing 
-    />
-  {/if}
-
-</div>
+</div><!-- /wrapper -->
