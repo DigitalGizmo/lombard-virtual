5d17a76abbe3687a8c635a8f91100a54508382de
Don Button <37458717+DigitalGizmo@users.noreply.github.com>
2024-11-06 16:29:51 -0500

    zero-fy the rotation frame numbers

--- src/components/ViewLombard.svelte
+++ src/components/ViewLombard.svelte
@@ -4,10 +4,20 @@
   export let showModal;
 
   let frame = 0;
+  let zFrame = '0';
+
+  $: if (frame < 10) {
+    zFrame = '00' + frame.toString();
+  } else if (frame < 100){
+    zFrame = '0' + frame.toString();
+  } else {
+    zFrame = frame.toString();
+  }
+  
 </script>
 
 <div >
-  <img src="{assetPath}images/rotate/Lombard00{frame}.png" 
+  <img src="{assetPath}images/rotate/Lombard{zFrame}.png" 
     alt="lombard hauler 3D view">
 </div>
   
@@ -16,7 +26,7 @@
   <p>{thisView.text} </p>
   Rotate: 
   <input 
-    type="range" min="0" max="9" bind:value={frame} />
+    type="range" min="0" max="100" bind:value={frame} />
   <p><a href="/" 
       on:click={(e) => { e.preventDefault(); showModal();}}>
       Learn more</a>
