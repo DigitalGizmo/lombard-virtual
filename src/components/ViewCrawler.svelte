40dfdea4e56112f15f2d04bf359e2b826ffdf439
Don Button <37458717+DigitalGizmo@users.noreply.github.com>
2025-02-10 16:36:03 -0500

    access track webp frames

--- src/components/ViewCrawler.svelte
+++ src/components/ViewCrawler.svelte
@@ -4,11 +4,23 @@
   export let showModal;
 
   let frame = 0;
+  let zFrame = '0';
+
+  $: if (frame < 10) {
+    zFrame = '00' + frame.toString();
+  } else if (frame < 100){
+    zFrame = '0' + frame.toString();
+  } else {
+    zFrame = frame.toString();
+  }
+  
 </script>
 
 <div >
-  <img src="{assetPath}images/views/crawler-track.jpg" 
-    alt="lombard crawler tracks">
+  <!-- <img src="{assetPath}images/tracks/tr001.jpg" 
+    alt="lombard crawler tracks"> -->
+    <img src="{assetPath}images/tracks/TR{zFrame}.webp" 
+    class="inset" alt="lombard crawler tracks">    
 </div>
   
 <div class="content {thisView.slug}">
@@ -21,6 +33,6 @@
     </a>
   </p>
   <label for="rotate-crawler">Rotate 3-D View:</label>
-  <input id="rotate-crawler" type="range" min="0" max="9" bind:value={frame} />
+  <input id="rotate-crawler" type="range" min="0" max="150" bind:value={frame} />
 
 </div>
