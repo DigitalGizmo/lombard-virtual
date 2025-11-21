b5f87a391c42829e791baf6b4f7e0c4c44b6a734
Juliet Jacobson <julietanna@users.noreply.github.com>
2025-11-21 15:28:17 -0500

    entered final text, incl credits.

--- src/components/Credits.svelte
+++ src/components/Credits.svelte
@@ -8,6 +8,32 @@
 
 <div >
   <h2>Credits</h2>
-  <p>Angela, Juliet, Don and Terry</p>
+      <h4>Funding for Virtual Lombard Development</h4>
+      <ul>
+        <li>Bill and Joan Alfond Foundation</li>
+        <li>Maine State Museum</li>
+      </ul>
+      <h4>Development Team</h4>
+      <ul>
+        <li>Terry Harper - Primary Author and 3D Graphics and Animation Developer; Historian and Curator, Maine Forest & Logging Museum</li>
+        <li>Angela Goebel-Bain - Curator of Decorative Arts and History, Maine State Museum</li>
+        <li>Juliet Jacobson - Design Director, Digital Gizmo</li>
+        <li>Don Button - Technical Director, Digital Gizmo</li>
+      </ul>
+      <h4>Content Advisors</h4>
+      <ul>
+        <li>Herb Crosby - Professor Emeritus of Mechanical Engineering Technology at University of Maine, Board of Directors, Maine Forest &amp; Logging Museum</li>
+        <li>Joanna Torow - Chief Educator, Maine State Museum</li>
+        <li>David Hunt - Lead Educator, Maine State Museum</li>
+        <li>Jessica Couture - Board of Directors Outreach Coordinator, Waterville Historical Society</li>
+        <li>Jay Violette – Board of Directors Executive Committee, Waterville Historical Society </li>
+      </ul>
+      <h4>Editorial Team</h4>
+      <ul>
+        <li>Bernard Fishman - Director, Maine State Museum</li>
+        <li>Sheila McDonald - Deputy Director, Maine State Museum</li>
+        <li>David Hunt - Lead Educator, Maine State Museum</li>
+        <li>Kate Webber - Lead Educator, Maine State Museum</li>
+      </ul>
 </div>
   
